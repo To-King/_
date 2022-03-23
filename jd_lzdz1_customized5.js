776381c8db53fbd5ab3b7a1ed31a76d5b080848d
king <ugg999999@gmail.com>
2022-03-23 12:40:41 +0800

    Initial commit

--- jd_lzdz1_customized5.js
+++ jd_lzdz1_customized5.js
@@ -56,7 +56,7 @@ if ($.isNode()) {
             $.bean = 0;
             $.ADID = getUUID('xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx', 1);
             $.UUID = getUUID('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
-            authorCodeList = []
+            authorCodeList = ["315823cf83aa4b19bb1af7cf156cc72d"]
             // $.authorCode = authorCodeList[random(0, authorCodeList.length)]
             $.authorCode = ownCode ? ownCode : authorCodeList[random(0, authorCodeList.length)]
             $.authorNum = `${random(1000000, 9999999)}`
