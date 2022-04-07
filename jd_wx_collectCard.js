f784ac63a3b130c71e799601489d9bd4410e2b0c
king <ugg999999@gmail.com>
2022-04-07 23:15:59 +0800

    Initial commit

--- jd_wx_collectCard.js
+++ jd_wx_collectCard.js
@@ -245,5 +245,5 @@ $.after = async function () {
         $.msg.push($.activityUrl);
     }
 }
-$.run({whitelist: ['1-5'], wait: [1000, 3000]}).catch(
+$.run({whitelist: ['1-11'], wait: [1000, 3000]}).catch(
     reason => $.log(reason));
