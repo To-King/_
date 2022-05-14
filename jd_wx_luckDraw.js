0f42f42917713da03ba9d60df6dbb512a8fddbc3
king <ugg999999@gmail.com>
2022-05-14 13:04:41 +0800

    Initial commit

--- jd_wx_luckDraw.js
+++ jd_wx_luckDraw.js
@@ -1,7 +1,3 @@
-//问题反馈:https://t.me/Wall_E_Channel
-/*
-7 7 7 7 7 m_jd_wx_luckDraw.js
-*/
 let mode = __dirname.includes('magic')
 const {Env} = mode ? require('./magic') : require('./magic')
 const $ = new Env('M幸运抽奖');
@@ -226,6 +222,5 @@ $.after = async function () {
     $.msg.push(message)
     $.msg.push($.activityUrl);
 }
-$.run(wait: [3000, 5000]).catch(
+$.run({whitelist: ['1-5'], wait: [3000, 5000]}).catch(
     reason => $.log(reason));
-
